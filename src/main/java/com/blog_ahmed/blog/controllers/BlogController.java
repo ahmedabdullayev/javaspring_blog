ea44541b29c6494b61be9f25aee7b3d55afbe2b5
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2022-02-01 23:16:16 +0200

    Add posts to db using form and @postmapping()

--- src/main/java/com/blog_ahmed/blog/controllers/BlogController.java
+++ src/main/java/com/blog_ahmed/blog/controllers/BlogController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -22,5 +25,13 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blog-main";
     }
-
+    @GetMapping("/blog/add")
+    public String BlogAdd(){
+        return "blog-add";
+    }
+    @PostMapping("/blog/add")
+    public String BlodPostAdd(Post model){
+        postRepository.save(model);
+        return "redirect:/blog";
+    }
 }
